ebc300e7c248c65f7ee4e0e7c959958cb8556918
Beena <cmbeena1@gmail.com>
2025-11-22 16:48:42 +0530

    this is commit froom asian time zone guy

--- src/demo/Demonew.java
+++ src/demo/Demonew.java
@@ -5,6 +5,8 @@ public class Demonew {
 	public static void main(String[] args) {
 
 		System.out.println("Hello world");
+		System.out.println("Hello world1");
+		System.out.println("Hello world2");
 
 	}
 
